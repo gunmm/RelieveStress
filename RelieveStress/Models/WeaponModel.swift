b562860c1fea96940ead3d64310be4b51c8bf4fa
闵哲 <mz@meitu.com>
2026-04-06 14:55:52 +0800

    【Feature】调整位置

--- RelieveStress/Models/WeaponModel.swift
+++ RelieveStress/Models/WeaponModel.swift
@@ -17,8 +17,8 @@ struct WeaponModel: Equatable {
     // Default available weapons
     static let availableWeapons: [WeaponModel] = [
         WeaponModel(id: "fist_small", name: "小拳头", damageValue: 2, iconEmoji: "🤜", impactLevel: .light),
-        WeaponModel(id: "fist_large", name: "大拳头", damageValue: 5, iconEmoji: "👊", impactLevel: .heavy),
         WeaponModel(id: "stick_small", name: "小棍子", damageValue: 4, iconEmoji: "🏏", impactLevel: .light),
+        WeaponModel(id: "fist_large", name: "大拳头", damageValue: 5, iconEmoji: "👊", impactLevel: .heavy),
         WeaponModel(id: "hammer_large", name: "大铁锤", damageValue: 10, iconEmoji: "🔨", impactLevel: .heavy)
     ]
     
